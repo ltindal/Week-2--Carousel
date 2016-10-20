3f54e959fb86be5f5932236418a7632218a6c8ec
Lauren <ltindal19@gmail.com>
2016-10-19 17:13:55 -0700

    Initial Commit

---
+++ Week 2- CarouselTests/Week_2__CarouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_2__CarouselTests.swift
+//  Week 2- CarouselTests
+//
+//  Created by Lauren Tindal on 10/19/16.
+//  Copyright © 2016 Lauren Tindal. All rights reserved.
+//
+
+import XCTest
+@testable import Week_2__Carousel
+
+class Week_2__CarouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
